b22bb0a1f34bffd05ec68611e7e6cf40f02310a1
Teddysir <sdzx0719@naver.com>
2025-12-18 19:38:48 +0900

    fix: main.css 경로 설정

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import "./assets/index.css"; // tailwind 및 shadcn 스타일
+import "./assets/main.css"; // tailwind 및 shadcn 스타일
 
 const app = createApp(App);
 
